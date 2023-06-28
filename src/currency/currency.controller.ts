8365a73a84955c27fafaff00d149085b7bda75ea
Wendel Bueres <wendelbueres@gmail.com>
2023-06-28 02:11:15 -0300

    feat: :sparkles: controller - currency

---
+++ src/currency/currency.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
+import { CurrencyService } from './currency.service';
+import { CreateCurrencyDto } from './dto/create-currency.dto';
+import { UpdateCurrencyDto } from './dto/update-currency.dto';
+
+@Controller('currency')
+export class CurrencyController {
+  constructor(private readonly currencyService: CurrencyService) {}
+
+  @Post()
+  create(@Body() createCurrencyDto: CreateCurrencyDto) {
+    return this.currencyService.create(createCurrencyDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.currencyService.findAll();
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCurrencyDto: UpdateCurrencyDto) {
+    return this.currencyService.update(id, updateCurrencyDto);
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.currencyService.remove(id);
+  }
+}
