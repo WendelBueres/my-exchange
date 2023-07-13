c0f1d8ec9b2be4b61d5ce121b4d7f5e07d61b978
Wendel Bueres <wendelbueres@gmail.com>
2023-07-13 01:41:42 -0300

    feat: :sparkles:

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -1,9 +1,35 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
+import authMiddleware from 'src/middlewares/auth.middleware';
+import isOwnerMiddleware from 'src/middlewares/isOwner.middleware';
 
 @Module({
   controllers: [WalletsController],
-  providers: [WalletsService]
+  providers: [WalletsService],
 })
-export class WalletsModule {}
+export class WalletsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(authMiddleware).forRoutes({
+      path: 'wallets',
+      method: RequestMethod.POST,
+    });
+    consumer.apply(authMiddleware).forRoutes({
+      path: 'wallets',
+      method: RequestMethod.GET,
+    });
+    consumer.apply(authMiddleware, isOwnerMiddleware).forRoutes({
+      path: 'wallets/:id',
+      method: RequestMethod.DELETE,
+    });
+    consumer.apply(authMiddleware, isOwnerMiddleware).forRoutes({
+      path: 'wallets/:id',
+      method: RequestMethod.PATCH,
+    });
+  }
+}
