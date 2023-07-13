b180be0e85d914448ac1a06e17ab79050491cee5
Wendel Bueres <wendelbueres@gmail.com>
2023-07-13 01:41:18 -0300

    feat: :sparkles: wallets service

--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateWalletDto } from './dto/create-wallet.dto';
-import { UpdateWalletDto } from './dto/update-wallet.dto';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 import { AppError } from 'src/errors';
 
 @Injectable()
@@ -17,93 +16,114 @@ export class WalletsService {
       throw new AppError('User not found');
     }
 
-    if (!createWalletDto.currency && !createWalletDto.iso4217) {
-      throw new AppError('Currency or iso4217 is required');
-    }
+    if (createWalletDto.currency || createWalletDto.iso4217) {
+      let currency = null;
 
-    let currency = null;
+      //Com tratamento de erro utilizando try/catch e instanceof Prisma.PrismaClientKnownRequestError
+      if (createWalletDto.currency) {
+        try {
+          currency = await this.prisma.currency.findUniqueOrThrow({
+            where: {
+              name: createWalletDto.currency,
+            },
+          });
+        } catch (error) {
+          if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025') {
+              throw new AppError('Currency not found', 404);
+            }
+          }
+        }
+      }
 
-    if (createWalletDto.currency) {
-      currency = await this.prisma.currency.findFirst({
+      //Tratamento de erro utilizando try/catch e instanceof Prisma.PrismaClientKnownRequestError
+      if (createWalletDto.iso4217) {
+        createWalletDto.iso4217 = createWalletDto.iso4217.toUpperCase();
+
+        try {
+          currency = await this.prisma.currency.findUniqueOrThrow({
+            where: {
+              iso4217: createWalletDto.iso4217,
+            },
+          });
+        } catch (error) {
+          if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025') {
+              throw new AppError('Currency not found', 404);
+            }
+          }
+        }
+      }
+
+      const walletExists = await this.prisma.wallet.count({
         where: {
-          name: createWalletDto.currency,
+          userId: userId,
+          currency: {
+            equals: currency.name,
+            mode: 'insensitive',
+          },
+        },
+      });
+
+      if (walletExists > 0) {
+        throw new AppError('Wallet already exists', 400);
+      }
+
+      return await this.prisma.wallet.create({
+        data: {
+          currency: currency.name,
+          value: createWalletDto.value,
+          valueBase: createWalletDto.valueBase,
+          User: {
+            connect: {
+              id: userId,
+            },
+          },
+          Currency: {
+            connect: {
+              id: currency.id,
+            },
+          },
         },
       });
     }
 
-    if (createWalletDto.iso4217) {
-      currency = await this.prisma.currency.findFirst({
-        where: {
-          iso4217: createWalletDto.iso4217,
-        },
-      });
-    }
-
-    return await this.prisma.wallet.create({
-      data: {
-        currency: createWalletDto.currency,
-        value: createWalletDto.value,
-        valueBase: createWalletDto.valueBase,
-        User: {
-          connect: {
-            id: userId,
-          },
-        },
-        Currency: {
-          connect: {
-            id: currency.id,
-          },
-        },
-      },
-    });
+    throw new AppError('Currency or iso4217 is required');
   }
 
   async findAll(userId: string) {
+    //TODO: Atualizar valores de moedas antes de retornar carteiras.
+
     return await this.prisma.wallet.findMany({
       where: { userId: userId },
     });
   }
 
   async findOne(id: string) {
+    //TODO: Atualizar valores de moedas antes de retornar carteira.
+
     const wallet = await this.prisma.wallet.findUnique({
       where: { id: id },
+      include: {
+        Extract: true,
+      },
     });
 
     if (!wallet) {
-      throw new AppError('Wallet not found');
+      throw new AppError('Wallet not found', 404);
     }
 
     return wallet;
   }
 
-  async findRegisters(id: string) {
-    return await this.prisma.register.findMany({
-      where: { walletId: id },
-    });
-  }
-
-  async update(id: string, updateWalletDto: UpdateWalletDto) {
-    const wallet = await this.prisma.wallet.findUnique({
-      where: { id: id },
-    });
-
-    if (!wallet) {
-      throw new AppError('Wallet not found');
-    }
-
-    return await this.prisma.wallet.update({
-      where: { id: id },
-      data: updateWalletDto,
-    });
-  }
-
+  //Tratamento de erro manual, utilizando condicional if
   async remove(id: string) {
     const wallet = await this.prisma.wallet.findUnique({
       where: { id: id },
     });
 
     if (!wallet) {
-      throw new AppError('Wallet not found');
+      throw new AppError('Wallet not found', 404);
     }
 
     return await this.prisma.wallet.delete({
