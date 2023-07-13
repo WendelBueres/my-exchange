c1faf256f3d5e139d69f951467bbd4f7710042d1
Wendel Bueres <wendelbueres@gmail.com>
2023-07-13 01:39:25 -0300

    feat: :sparkles: currency service

--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -9,6 +9,32 @@ export class CurrencyService {
   prisma = new PrismaClient();
 
   async create(createCurrencyDto: CreateCurrencyDto) {
+    if (createCurrencyDto.iso4217) {
+      createCurrencyDto.iso4217 = createCurrencyDto.iso4217.toUpperCase();
+
+      const checkIfISO4217Exists = await this.prisma.currency.findFirst({
+        where: {
+          iso4217: { equals: createCurrencyDto.iso4217, mode: 'insensitive' },
+        },
+      });
+
+      if (checkIfISO4217Exists) {
+        throw new AppError('iso4217 already is registered', 400);
+      }
+    }
+
+    if (createCurrencyDto.name) {
+      const checkIfNameExists = await this.prisma.currency.findFirst({
+        where: {
+          name: { equals: createCurrencyDto.name, mode: 'insensitive' },
+        },
+      });
+
+      if (checkIfNameExists) {
+        throw new AppError('name already is registered', 400);
+      }
+    }
+
     return await this.prisma.currency.create({
       data: {
         name: createCurrencyDto.name,
@@ -28,6 +54,35 @@ export class CurrencyService {
 
     if (!currency) {
       throw new AppError('Currency not found', 404);
+    }
+
+    const checkIfISO4217Exists = await this.prisma.currency.findFirst({
+      where: {
+        iso4217: { equals: updateCurrencyDto.iso4217, mode: 'insensitive' },
+      },
+    });
+
+    if (updateCurrencyDto.iso4217) {
+      updateCurrencyDto.iso4217 = updateCurrencyDto.iso4217.toUpperCase();
+
+      if (
+        checkIfISO4217Exists &&
+        checkIfISO4217Exists.iso4217 !== currency.iso4217
+      ) {
+        throw new AppError('iso4217 already is registered', 400);
+      }
+    }
+
+    if (updateCurrencyDto.name) {
+      const checkIfNameExists = await this.prisma.currency.findFirst({
+        where: {
+          name: { equals: updateCurrencyDto.name, mode: 'insensitive' },
+        },
+      });
+
+      if (checkIfNameExists && checkIfNameExists.name !== currency.name) {
+        throw new AppError('name already is registered', 400);
+      }
     }
 
     return await this.prisma.currency.update({
