4dc982c325dcdc56d7e5c92c558231eb6846fa01
Wendel Bueres <wendelbueres@gmail.com>
2023-07-13 01:43:20 -0300

    refactor: :hammer: api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.URL_BASE_API,
-  data: {
-    apiKey: process.env.API_KEY,
-  },
+  timeout: 200000,
 });
 
 export default api;
