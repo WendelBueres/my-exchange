f5165026dc9b15d4642b32adf4cefc1de5a1a015
Wendel Bueres <wendelbueres@gmail.com>
2023-07-13 01:38:48 -0300

    feat: :sparkles: criado EnumCurrency

--- src/currency/dto/create-currency.dto.ts
+++ src/currency/dto/create-currency.dto.ts
@@ -1,9 +1,44 @@
-import { IsString } from "class-validator";
+import { IsEnum, IsString } from 'class-validator';
 
+enum EnumCurrency {
+  'EUR' = 'EUR',
+  'USD' = 'USD',
+  'JPY' = 'JPY',
+  'BGN' = 'BGN',
+  'CZK' = 'CZK',
+  'DKK' = 'DKK',
+  'GBP' = 'GBP',
+  'HUF' = 'HUF',
+  'PLN' = 'PLN',
+  'RON' = 'RON',
+  'SEK' = 'SEK',
+  'CHF' = 'CHF',
+  'ISK' = 'ISK',
+  'NOK' = 'NOK',
+  'HRK' = 'HRK',
+  'RUB' = 'RUB',
+  'TRY' = 'TRY',
+  'AUD' = 'AUD',
+  'BRL' = 'BRL',
+  'CAD' = 'CAD',
+  'CNY' = 'CNY',
+  'HKD' = 'HKD',
+  'IDR' = 'IDR',
+  'ILS' = 'ILS',
+  'INR' = 'INR',
+  'KRW' = 'KRW',
+  'MXN' = 'MXN',
+  'MYR' = 'MYR',
+  'NZD' = 'NZD',
+  'PHP' = 'PHP',
+  'SGD' = 'SGD',
+  'THB' = 'THB',
+  'ZAR' = 'ZAR',
+}
 export class CreateCurrencyDto {
-    @IsString()
-    name: string;
-    
-    @IsString()
-    iso4217: string;
+  @IsString()
+  name: string;
+
+  @IsEnum(EnumCurrency)
+  iso4217: string;
 }
