7f9f38cb8499888c052fcfae33af290b208d4e3c
Wendel Bueres <wendelbueres@gmail.com>
2023-07-28 02:25:50 -0300

    refactor: :hammer: delete controller Patch

--- src/registers/registers.controller.ts
+++ src/registers/registers.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
 import { RegistersService } from './registers.service';
 import { CreateRegisterDto } from './dto/create-register.dto';
-import { UpdateRegisterDto } from './dto/update-register.dto';
 import { Request } from 'express';
 
 @Controller('registers')
@@ -11,11 +10,6 @@ export class RegistersController {
   @Post()
   create(@Body() createRegisterDto: CreateRegisterDto, @Req() req: Request) {
     return this.registersService.create(createRegisterDto, req.user);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRegisterDto: UpdateRegisterDto, @Req() req: Request) {
-    return this.registersService.update(id, updateRegisterDto, req.user);
   }
 
   @Delete(':id')
