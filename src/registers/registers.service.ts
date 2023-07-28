eaff59082b6b13096904fbba7fc0963d8ad8f507
Wendel Bueres <wendelbueres@gmail.com>
2023-07-28 02:26:11 -0300

    refactor: :hammer: import utils

--- src/registers/registers.service.ts
+++ src/registers/registers.service.ts
@@ -1,32 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRegisterDto } from './dto/create-register.dto';
-import { UpdateRegisterDto } from './dto/update-register.dto';
-import { PrismaClient, User } from '@prisma/client';
+import { EnumTypeRegister, PrismaClient, User } from '@prisma/client';
 import { AppError } from 'src/errors';
-import api from 'src/services/api';
+import defineValueDay from 'src/utils/defineValueDay.utils';
+import checkWallet from 'src/utils/checkWallet.utils';
+import refreshValuesWallet from 'src/utils/refreshValuesWallet.utils';
 
 @Injectable()
 export class RegistersService {
   prisma = new PrismaClient();
 
   async create(createRegisterDto: CreateRegisterDto, user: User) {
-    const wallet = await this.prisma.wallet.findUnique({
+    let wallet = await this.prisma.wallet.findUnique({
       where: { id: createRegisterDto.walletId },
     });
 
-    if (!wallet) {
-      throw new AppError('Wallet not found', 404);
-    }
-
-    if (wallet.userId !== user.id) {
-      throw new AppError('Wallet not found', 404);
-    }
+    checkWallet(wallet, user);
+    wallet = await refreshValuesWallet(wallet);
 
     const currency = await this.prisma.currency.findUnique({
       where: { id: wallet.currencyId },
     });
-
-    let currencyResponse = null;
 
     if (!createRegisterDto.valueBase && !createRegisterDto.valueCurrent) {
       throw new AppError('Value base or value current is required');
@@ -36,123 +30,94 @@ export class RegistersService {
       throw new AppError('Value base or value current is required, not both');
     }
 
-    if (createRegisterDto.valueCurrent) {
-      let consult = `${currency.iso4217}-BRL`;
-      await api
-        .get(consult)
-        .then((response) => (currencyResponse = response.data))
-        .catch((error) => {
-          throw new AppError(error.message, error.statusCode);
-        });
-      consult = consult.replace('-', '');
-      currencyResponse = parseFloat(currencyResponse[consult].bid).toFixed(4);
-      currencyResponse = parseFloat(currencyResponse);
-
-      createRegisterDto.valueBase =
-        createRegisterDto.valueCurrent * currencyResponse;
-    } else {
-      async function defineValueDay() {
-        let consult = `BRL-${currency.iso4217}`;
-        await api
-          .get(consult)
-          .then((response) => (currencyResponse = response.data))
-          .catch((error) => {
-            console.log(error.message, error.statusCode);
-          });
-        consult = consult.replace('-', '');
-        currencyResponse = parseFloat(currencyResponse[consult].bid);
-        currencyResponse = currencyResponse.toFixed(4);
-        currencyResponse = parseFloat(currencyResponse);
-
-        createRegisterDto.valueCurrent =
-          currencyResponse * createRegisterDto.valueBase;
-
-        consult = `${currency.iso4217}-BRL`;
-        await api
-          .get(consult)
-          .then((response) => (currencyResponse = response.data))
-          .catch((error) => {
-            console.log(error.message, error.statusCode);
-          });
-        consult = consult.replace('-', '');
-
-        currencyResponse = parseFloat(currencyResponse[consult].bid).toFixed(4);
-
-        return parseFloat(currencyResponse);
-      }
-
-      currencyResponse = await defineValueDay();
-    }
+    let currencyResponse: number = await defineValueDay(
+      createRegisterDto,
+      currency,
+    );
 
     const register = await this.prisma.register.create({
       data: {
         type: createRegisterDto.type,
         valueBase: createRegisterDto.valueBase,
         valueCurrent: createRegisterDto.valueCurrent,
-        valueDayPurchase: currencyResponse,
+        purchaseExchange: currencyResponse,
         Wallet: {
           connect: { id: createRegisterDto.walletId },
         },
       },
     });
 
-    await this.prisma.wallet.update({
-      where: { id: createRegisterDto.walletId },
-      data: {
-        value: wallet.value + register.valueCurrent,
-        valueBase: wallet.valueBase + register.valueBase,
-      },
-    });
+    if (createRegisterDto.type === EnumTypeRegister.PURCHASE) {
+      const value = (wallet.value + register.valueCurrent).toFixed(2);
+      const valueBase = (wallet.valueBase + register.valueBase).toFixed(2);
+      await this.prisma.wallet.update({
+        where: { id: createRegisterDto.walletId },
+        data: {
+          value: parseFloat(value),
+          valueBase: parseFloat(valueBase),
+        },
+      });
+    }
+
+    if (createRegisterDto.type === EnumTypeRegister.SALE) {
+      const value = (wallet.value - register.valueCurrent).toFixed(2);
+      const valueBase = (wallet.valueBase - register.valueBase).toFixed(2);
+
+      if (parseFloat(value) < 0 || parseFloat(valueBase) < 0) {
+        await this.prisma.register.delete({
+          where: { id: register.id },
+        });
+
+        throw new AppError('Insufficient funds');
+      }
+
+      await this.prisma.wallet.update({
+        where: { id: createRegisterDto.walletId },
+        data: {
+          value: parseFloat(value),
+          valueBase: parseFloat(valueBase),
+        },
+      });
+    }
 
     return register;
   }
 
-  async update(id: string, updateRegisterDto: UpdateRegisterDto, user: User) {
-    //TODO: Fazer lógica para atualizar o valor da carteira, caso o valor do registro seja alterado.
-
-    const register = await this.prisma.register.findUnique({
-      where: { id: id },
-    });
-
-    await this.prisma.wallet.update({
-      where: { id: register.walletId },
-      data: {
-        value: register.valueCurrent - register.valueDayPurchase,
-        valueBase: register.valueBase - register.valueBase,
-      },
-    });
-
-    await this.prisma.register.delete({
-      where: { id: id },
-    });
-
-    const wallet = await this.prisma.wallet.findUnique({
-      where: { id: register.walletId },
-    });
-
-    if (wallet.userId !== user.id) {
-      throw new AppError('Register not found', 404);
-    }
-
-    return this.prisma.register.update({
-      where: { id: id },
-      data: updateRegisterDto,
-    });
-  }
-
   async remove(id: string, user: User) {
-    //TODO: Fazer lógica para atualizar o valor da carteira, caso o valor do registro seja excluido.
-
     const register = await this.prisma.register.findUnique({
       where: { id: id },
     });
 
-    const wallet = await this.prisma.wallet.findUnique({
+    let wallet = await this.prisma.wallet.findUnique({
       where: { id: register.walletId },
     });
 
-    if (wallet.userId !== user.id) {
-      throw new AppError('Register not found', 404);
+    checkWallet(wallet, user);
+    wallet = await refreshValuesWallet(wallet);
+
+    if (register.type === EnumTypeRegister.PURCHASE) {
+      const value = (wallet.value - register.valueCurrent).toFixed(2);
+      const valueBase = (wallet.valueBase - register.valueBase).toFixed(2);
+
+      await this.prisma.wallet.update({
+        where: { id: register.walletId },
+        data: {
+          value: parseFloat(value),
+          valueBase: parseFloat(valueBase),
+        },
+      });
+    }
+    if (register.type === EnumTypeRegister.SALE) {
+      const value = (wallet.value + register.valueCurrent).toFixed(2);
+      const valueBase = (wallet.valueBase + register.valueBase).toFixed(2);
+
+      await this.prisma.wallet.update({
+        where: { id: register.walletId },
+        data: {
+          value: parseFloat(value),
+          valueBase: parseFloat(valueBase),
+        },
+      });
     }
 
     return await this.prisma.register.delete({
